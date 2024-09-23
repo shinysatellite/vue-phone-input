b7733b5f6f8d7316af7bc4d005c570bc8e3f6d62
Jakub Zielinski <jakubzileinski922@gmail.com>
2024-09-23 19:16:54 +0200

    refactor vue project envronment

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
-<script lang="ts" setup>
-import { ref } from 'vue';
-import PhoneInput from './components/PhoneInput.vue';
-
-const phone = ref('');
-</script>
-
 <template>
   <div>
     <PhoneInput v-model="phone" />
     <div>Entered Phone Number: {{ phone }}</div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import PhoneInput from "./components/PhoneInput.vue";
+
+const phone = ref("");
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/components/PhoneInput.vue
+++ src/components/PhoneInput.vue
@@ -1,36 +1,3 @@
-<script lang="ts" setup>
-import { ref, nextTick, computed } from 'vue';
-
-const props = defineProps<{
-  modelValue: string;
-}>();
-
-const emit = defineEmits(['update:modelValue']);
-
-const inputValue = ref<string>(props.modelValue);
-const phoneInput = ref<HTMLInputElement | null>(null);
-
-const placeholder = computed<string>(() => {
-  return navigator.language === 'en-US' ? '+1 (123) 456-7890' : 'Phone number';
-});
-
-function handleInput(event: Event) {
-  const inputElement = event.target as HTMLInputElement;
-  const rawInput = inputElement.value;
-  const allowed = rawInput.replace(/[^0-9+()\- ]/g, '');
-  inputValue.value = allowed;
-
-  nextTick(() => {
-    if (phoneInput.value) {
-      phoneInput.value.value = allowed;
-    }
-  });
-
-  emit('update:modelValue', allowed);
-}
-
-</script>
-
 <template>
   <input
     ref="phoneInput"
@@ -39,3 +6,35 @@ function handleInput(event: Event) {
     :placeholder="placeholder"
   />
 </template>
+
+<script lang="ts" setup>
+import { ref, nextTick, computed, defineEmits, defineProps } from "vue";
+
+const props = defineProps<{
+  modelValue: string;
+}>();
+
+const emit = defineEmits(["update:modelValue"]);
+
+const inputValue = ref<string>(props.modelValue);
+const phoneInput = ref<HTMLInputElement | null>(null);
+
+const placeholder = computed<string>(() => {
+  return navigator.language === "en-US" ? "+1 (123) 456-7890" : "Phone number";
+});
+
+function handleInput(event: Event) {
+  const inputElement = event.target as HTMLInputElement;
+  const rawInput = inputElement.value;
+  const allowed = rawInput.replace(/[^0-9+()\- ]/g, "");
+  inputValue.value = allowed;
+
+  nextTick(() => {
+    if (phoneInput.value) {
+      phoneInput.value.value = allowed;
+    }
+  });
+
+  emit("update:modelValue", allowed);
+}
+</script>
